12c79721787ccea80815390c3991eec8adc04e65
SwadhiSankar <swadhis14@gmail.com>
2024-08-06 21:15:58 +0530

    not found feature for routes **

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -25,4 +26,9 @@ export const routes: Routes = [
       },
     ],
   },
+  {
+    path : '**',
+    component: NotFoundComponent
+
+  }
 ];
